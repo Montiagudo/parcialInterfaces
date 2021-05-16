c8ac6af113b836a652502c68d22cc12c5a1d26c0
DanielMontiagudo <montiagudo.daniel@gmail.com>
2021-05-16 08:19:52 -0600

    parcial finalizado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Page1Component } from './Components/page1/page1.component';
+import { Page2Component } from './Components/page2/page2.component';
+import { Page3Component } from './Components/page3/page3.component';
+import { Page4Component } from './Components/page4/page4.component';
+import { Page5Component } from './Components/page5/page5.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Page1Component,
+    Page2Component,
+    Page3Component,
+    Page4Component,
+    Page5Component
   ],
   imports: [
     BrowserModule,
